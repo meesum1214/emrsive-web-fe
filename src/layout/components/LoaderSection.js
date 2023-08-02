108fd033b1b6b2811b2dfe7c11241c11e3e930eb
meesum1214 <r1f18bscs0010@ucp.edu.pk>
2023-08-02 23:52:16 +0500

    loader setting

--- src/layout/components/LoaderSection.js
+++ src/layout/components/LoaderSection.js
@@ -1,9 +1,10 @@
-import { Loader } from "@mantine/core"
+import { Image, Loader } from "@mantine/core"
 
 export default ({ state, style }) => {
     return (
         state && <div className={`${style} fixed top-0 left-0 w-screen h-screen bg-[#ffffffbb] flex justify-center items-center z-50`}>
-            <Loader color="orange" />
+            {/* <Loader color="indigo" /> */}
+            <Image src="/favicon.png" width={50} className="spinner" />
         </div>
     )
 }
